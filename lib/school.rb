85d844d9da1b57448d9d4a01a960c21e2c6bea4a
Wendi Chen <epanistamai.proles@gmail.com>
2018-12-11 20:59:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end  
+  
+  
+  ROSTER = Hash.new
+  
+  def add_student(name, grade)
+    ROSTER[grade] = name
+  end
+  
+  def grade(num)
+    ROSTER[num]
+  end
+  
+  def sort
+    
+  end
+  
+end
