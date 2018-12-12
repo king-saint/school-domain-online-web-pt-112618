d0a9d76b45bbf3d04186f10a204fbb46491fb5b5
Wendi Chen <epanistamai.proles@gmail.com>
2018-12-12 17:58:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+  
+  def name
+    @name
+  end
+  
+  
+  
+  def add_student(name, grade)
+    self.roster[grade] = [] 
+    @roster[grade] << name
+  end
+  
+end
