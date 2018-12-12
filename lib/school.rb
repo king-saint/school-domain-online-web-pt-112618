7a73fc2d7d8ab194430d8a2d75523460518c5613
Wendi Chen <epanistamai.proles@gmail.com>
2018-12-12 01:39:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    arr = []
+    roster[grade] 
+    arr.push name
+    roster[grade] = arr
+  end
+  
+end
