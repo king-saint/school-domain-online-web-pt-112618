f088c59e28f75e1ea9cebb2dc2c5d2de7e59f113
Wendi Chen <epanistamai.proles@gmail.com>
2018-12-12 22:01:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+  
+  def name
+    @name
+  end
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else 
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+  
+  def grade(num)
+    @roster[num]
+  end
+  
+  def sort
+    @roster.collect do |key, value|
+      "#{key} => #{value.sort}"
+    end
+  end
+  
+end
